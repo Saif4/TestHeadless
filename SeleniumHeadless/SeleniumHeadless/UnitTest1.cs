9bc9d94633830431d511ca89bb12a082c7b397c8
Saif Alhindosh <saif.alhindosh@evry.com>
2019-10-16 14:32:01 +0200

    new commit

--- SeleniumHeadless/SeleniumHeadless/UnitTest1.cs
+++ SeleniumHeadless/SeleniumHeadless/UnitTest1.cs
@@ -14,20 +14,14 @@ namespace Selenium
         {
             var options = new ChromeOptions();
             options.AddArguments("headless");
-          
             var browser = new ChromeDriver(options);
 
-            
             browser.Navigate().GoToUrl("http://www.apple.com");
-            //  browser.FindElementByName("Google").SendKeys("Aftonbladet");
-            // browser.FindElementByName("Sök åå Google").Click();
             var t = browser.Title;
             Console.WriteLine(t);
             browser.FindElementByLinkText("Mac").Click();
             var url = browser.Url;
             Assert.AreEqual(url, "https://www.apple.com/mac/");
-
-
         }
  
     }
